286b8cc8f9e86c81edc968fd882e255c09d52f41
Josh Yuan <joshua.yuano@gmail.com>
2016-02-19 17:34:30 -0800

    format clean up.

--- src/main/java/com/addressbook/dao/AddressBookDAO.java
+++ src/main/java/com/addressbook/dao/AddressBookDAO.java
@@ -8,39 +8,39 @@ import org.hibernate.SessionFactory;
 import java.util.List;
 
 public class AddressBookDAO extends AbstractDAO<Student> {
-	
-    public AddressBookDAO(SessionFactory sessionFactory) {
-        super(sessionFactory);
-    }
-    
-    public Optional<Student> findById(long id) {
-        return Optional.fromNullable(get(id));
-    }
-    
-    public Student create(Student student) {
-        return persist(student);
-    }
-    
-    public List<Student> findAll() {
-        return list(namedQuery("com.addressbook.api.application.pojo.Student.findAll"));
-    }
+
+	public AddressBookDAO(SessionFactory sessionFactory) {
+		super(sessionFactory);
+	}
+
+	public Optional<Student> findById(long id) {
+		return Optional.fromNullable(get(id));
+	}
+
+	public Student create(Student student) {
+		return persist(student);
+	}
+
+	public List<Student> findAll() {
+		return list(namedQuery("com.addressbook.api.application.pojo.Student.findAll"));
+	}
 
 	public List<Student> findByName(String name) {
 		StringBuilder builder = new StringBuilder("%");
 		builder.append(name).append("%"); // %name%
-		return list(namedQuery("com.addressbook.api.application.pojo.Student.findByName").
-				setParameter("name", builder.toString()));
+		return list(namedQuery("com.addressbook.api.application.pojo.Student.findByName").setParameter("name",
+				builder.toString()));
 	}
-	
+
 	public List<Student> findByParent(String parent) {
 		StringBuilder builder = new StringBuilder("%");
 		builder.append(parent).append("%"); // %Parent%
-		return list(namedQuery("com.addressbook.api.application.pojo.Student.findByParent").
-				setParameter("parent", builder.toString()));
+		return list(namedQuery("com.addressbook.api.application.pojo.Student.findByParent").setParameter("parent",
+				builder.toString()));
 	}
-	
+
 	public List<Student> findByGrade(int grade) {
-		return list(namedQuery("com.addressbook.api.application.pojo.Student.findByGrade").
-				setParameter("grade", grade));
+		return list(
+				namedQuery("com.addressbook.api.application.pojo.Student.findByGrade").setParameter("grade", grade));
 	}
 }
